4b0dda2b3717455c2301cfe9b9fe7bda3a4ffff9
Rodion Sibov <rodionsibov@gmail.com>
2021-12-15 16:52:07 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     // history: createWebHistory(process.env.BASE_URL),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: About
+        }
+    ]
 })
 
 // router.beforeEach((to, from, next) => {

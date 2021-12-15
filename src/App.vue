32085bd37b67300066c54868e78c3e890b554aa8
Rodion Sibov <rodionsibov@gmail.com>
2021-12-15 17:24:00 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <br>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-
-const router = createRouter({
-    history: createWebHistory(),
-    // history: createWebHistory(process.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'About',
-            component: About
-        }
-    ]
-})
-
-// router.beforeEach((to, from, next) => {
-//     document.title = `${to.name} | Product and Cart`
-//     next()
-//   })
+import router from './router'
 
 createApp(App).use(router).mount('#app')

23c56b5b4d458817083eec2ff92fe9903cb55adf
Rodion Sibov <rodionsibov@gmail.com>
2021-12-16 17:03:28 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
   <div id="nav" class="bg-gray-700 text-gray-400 p-5 flex gap-4 text-sm border-b-4">
     <router-link class="hover:underline" to="/">Home</router-link>
     <router-link class="hover:underline" to="/brazil">Brazil</router-link>
+    <router-link class="hover:underline" to="/panama">Panama</router-link>
     <router-link class="hover:underline" to="/hawaii">Hawaii</router-link>
     <router-link class="hover:underline" to="/jamaica">Jamaica</router-link>
-    <router-link class="hover:underline" to="/panama">Panama</router-link>
     <router-link class="hover:underline" to="/about">About</router-link>
   </div>
   <div class="p-10">
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="">
-        <h1>About</h1>
-    </div>
+  <div class="">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+      doloremque dolores fuga dolorum impedit dolor eum repudiandae. Inventore
+      natus voluptatum earum consectetur consequuntur! Porro veniam consequatur
+      eligendi ad obcaecati nam.
+    </p>
+  </div>
 </template>

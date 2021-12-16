9cd0a25ed4295ee730e43f975918de44f9b498d7
Rodion Sibov <rodionsibov@gmail.com>
2021-12-16 11:37:22 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <br />
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link> |
-  <router-link to="/brazil">Brazil</router-link>
+  <div class="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/brazil">Brazil</router-link>
+  </div>
   <div class="container">
     <router-view></router-view>
   </div>

edb6e6595ab54dd47686b1a13ae83fb2bfdb2116
Rodion Sibov <rodionsibov@gmail.com>
2021-12-17 12:43:34 +0200

    init

---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>
+      {{ destination.name }}
+    </h1>
+    <div>
+      <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      <p>
+        {{ destination.description }}
+      </p>
+    </div>
+  </div>
+</template> 
+
+<script>
+import sourceData from "./../data.json";
+
+export default {
+  computed: {
+    destinationId() {
+      return parseInt(this.$route.params.id);
+    },
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.destinationId
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>

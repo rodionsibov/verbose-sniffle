a97c90c3e8ee8b0614a53f5141a93b6a24f8fda4
Rodion Sibov <rodionsibov@gmail.com>
2021-12-17 11:18:22 +0200

    init

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav" class="bg-gray-700 text-gray-400 p-5 flex gap-4 text-sm border-b-4">
+    <router-link class="hover:underline" to="/">Home</router-link>
+    <router-link class="hover:underline" to="/brazil">Brazil</router-link>
+    <router-link class="hover:underline" to="/panama">Panama</router-link>
+    <router-link class="hover:underline" to="/hawaii">Hawaii</router-link>
+    <router-link class="hover:underline" to="/jamaica">Jamaica</router-link>
+    <router-link class="hover:underline" to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

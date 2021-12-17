df35d88b2eaed51d84f318d519097265561acb14
Rodion Sibov <rodionsibov@gmail.com>
2021-12-17 14:38:50 +0200

    update

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,18 +1,31 @@
 <template>
-  <div id="nav" class="bg-gray-700 text-gray-400 p-5 flex gap-4 text-sm border-b-4">
-    <router-link class="hover:underline" to="/">Home</router-link>
-    <router-link class="hover:underline" to="/brazil">Brazil</router-link>
-    <router-link class="hover:underline" to="/panama">Panama</router-link>
-    <router-link class="hover:underline" to="/hawaii">Hawaii</router-link>
-    <router-link class="hover:underline" to="/jamaica">Jamaica</router-link>
-    <router-link class="hover:underline" to="/about">About</router-link>
+  <div
+    class="bg-gray-700 text-gray-400 p-5 flex items-center gap-4 text-sm border-b-4"
+  >
+  <router-link to="/" class="text-xl font-bold">
+    Vue School Travel App
+  </router-link>
+    <router-link
+      class="hover:underline"
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="{ name: 'Destination', params: { id: destination.id } }"
+    >
+      {{ destination.name }}
+    </router-link>
   </div>
 </template>
 
 <script>
-export default {
+import sourceData from "./../data.json";
 
-}
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
 </script>
 
 <style scoped>

caad92e7a4f49bcfce635485030522e6ee319501
Rodion Sibov <rodionsibov@gmail.com>
2021-12-19 21:43:47 +0200

    init

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Not Found</h1>
+  <p>
+    Ooops, we couldn't find that page. Try going
+    <router-link to="/">home</router-link>
+  </p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
